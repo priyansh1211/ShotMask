fbee446b7fecdd0ff1512d8c3e72cc705e18d4fe
priyansh1211 <priyanshbhatt1112@gmail.com>
2026-06-03 17:50:17 +0530

    Added comments for better understanding

--- src/sam2_predictor.py
+++ src/sam2_predictor.py
@@ -3,9 +3,9 @@ import numpy as np
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 
-checkpoint = "checkpoints/sam2_hiera_tiny.pt"
-model_cfg = "configs/sam2.1/sam2.1_hiera_t.yaml"
+checkpoint = "checkpoints/sam2_hiera_tiny.pt"  ## Model weights
+model_cfg = "configs/sam2.1/sam2.1_hiera_t.yaml" ##the model configuration file
 
-model = build_sam2(checkpoint=checkpoint, model_cfg=model_cfg)
-predictor = SAM2ImagePredictor(model)
+model = build_sam2(checkpoint=checkpoint, model_cfg=model_cfg) ## build the model and it returns a model
+predictor = SAM2ImagePredictor(model) ## build the predictor and it returns a predictor
 print("Model loaded successfully.")
