e95f628d6d0f41a28ab1f4389e7b7edba2752b80
priyansh1211 <priyanshbhatt1112@gmail.com>
2026-06-03 18:30:54 +0530

    Updated the SAM2 Tiny Model version

--- src/sam2_predictor.py
+++ src/sam2_predictor.py
@@ -3,7 +3,7 @@ import numpy as np
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 
-checkpoint = "checkpoints/sam2_hiera_tiny.pt"  ## Model weights
+checkpoint = "checkpoints/sam2.1_hiera_tiny.pt"  ## Model weights
 model_cfg = "configs/sam2.1/sam2.1_hiera_t.yaml" ##the model configuration file
 
 model = build_sam2(config_file=model_cfg, ckpt_path=checkpoint) ## build the model and it returns a model
