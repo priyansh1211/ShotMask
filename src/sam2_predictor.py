ecfc81eac7d541e5755b6881c9c3ab66279c8bd1
priyansh1211 <priyanshbhatt1112@gmail.com>
2026-06-03 18:25:12 +0530

    Corrected the order of parameter in build_sam2 function

--- src/sam2_predictor.py
+++ src/sam2_predictor.py
@@ -6,6 +6,6 @@ from sam2.sam2_image_predictor import SAM2ImagePredictor
 checkpoint = "checkpoints/sam2_hiera_tiny.pt"  ## Model weights
 model_cfg = "configs/sam2.1/sam2.1_hiera_t.yaml" ##the model configuration file
 
-model = build_sam2(checkpoint=checkpoint, model_cfg=model_cfg) ## build the model and it returns a model
+model = build_sam2(config_file=model_cfg, ckpt_path=checkpoint) ## build the model and it returns a model
 predictor = SAM2ImagePredictor(model) ## build the predictor and it returns a predictor
 print("Model loaded successfully.")
